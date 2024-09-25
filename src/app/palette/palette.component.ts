8da86542792f8801333d2f62fada6b5905c3b680
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2024-09-25 06:13:12 +0100

    menu front end

--- src/app/palette/palette.component.ts
+++ src/app/palette/palette.component.ts
@@ -3,6 +3,23 @@ import { PaletteService } from './palette.service';
 import { RandomRecipeDto } from './random-recipe.dto';
 import { LoggerModule, NGXLogger } from 'ngx-logger';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  IonCard,
+  IonCardSubtitle,
+  IonCardContent,
+  IonCardTitle,
+  IonCardHeader,
+  IonContent,
+  IonChip,
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+
+export enum LengthGauge {
+  Short = 'Short',
+  Medium = 'Medium',
+  Long = 'Long',
+  Unknown = 'Unknown',
+}
 
 @Component({
   selector: 'app-palette',
@@ -10,7 +27,17 @@ import { HttpClientModule } from '@angular/common/http';
   encapsulation: ViewEncapsulation.None,
   standalone: true,
   providers: [PaletteService],
-  imports: [HttpClientModule],
+  imports: [
+    IonChip,
+    IonContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCard,
+    HttpClientModule,
+    CommonModule,
+  ],
 })
 export class PaletteComponent implements OnInit {
   recipes: RandomRecipeDto[] = [];
@@ -20,8 +47,8 @@ export class PaletteComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log('HELLLO');
-    this.getRandomRecipes(2);
+    console.log('hello');
+    this.getRandomRecipes(5);
   }
 
   getRandomRecipes(numberOfRecipes: number) {
@@ -34,5 +61,43 @@ export class PaletteComponent implements OnInit {
         // TODO: put toaster up
       },
     });
+  }
+
+  prettifyRecipeName(name: string): string {
+    return (
+      name.split('-').join(' ').charAt(0).toUpperCase() +
+      name.split('-').join(' ').slice(1).toLowerCase()
+    );
+  }
+
+  prettifyPrepTime(prepTime: string): string {
+    return prepTime.split('T')[1].split('M')[0] + ' minutes';
+  }
+
+  colorPrepTime(prepTime: string): LengthGauge {
+    const time = parseInt(prepTime.split('T')[1].split('M')[0]);
+    if (isNaN(time)) {
+      return LengthGauge.Unknown;
+    }
+
+    if (time <= 20) {
+      return LengthGauge.Short;
+    } else if (time <= 45) {
+      return LengthGauge.Medium;
+    } else {
+      return LengthGauge.Long;
+    }
+  }
+
+  giveMeSimpleIngredientList(ingredients: string[]): string {
+    const ingredientNames = ingredients.map((val: string): string => {
+      return val.split(' ').slice(2).join(' ');
+    });
+
+    const notWaterIngredients = ingredientNames.filter(
+      (word) => !word.startsWith('Water')
+    );
+
+    return notWaterIngredients.join(', ');
   }
 }
