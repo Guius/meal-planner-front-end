b7959914cb775b630852f8ad4e6f77f9ec0af176
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2025-10-01 18:47:47 +0100

    MP-73 adding action colors to form button

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         subtitle: "#f9eee2",
         "content-text-grey": "#616161",
         button_bg: "#fdf4f3",
+        button_bg_hover: "#f7e6e4",
+        button_bg_active: "#f1d5d2",
+        button_bg_focus: "#f9eae8",
+        button_bg_disabled: "#f5f5f5",
       },
       borderColor: {
         "content-text-grey": "#D0D0D0",
